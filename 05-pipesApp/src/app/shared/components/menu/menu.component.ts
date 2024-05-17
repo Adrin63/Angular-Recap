59c9e75280d2f61a6dedd1f75a5c17061b04bf49
Adrin63 <adrianriscolopez@gmail.com>
2024-05-17 19:47:10 +0200

    new proyect start

---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``
+})
+export class MenuComponent {
+   public menuItems: MenuItem[] = [];
+
+   ngOnInit(){
+    this.menuItems = [
+      {label: 'New', icon: 'pi pi-fw pi-plus'},
+      {label: 'Open', icon: 'pi pi-fw pi-download'},
+      {label: 'Undo', icon: 'pi pi-fw pi-refresh'},
+    ];
+   }
+}
