861b2472b1653c00e52c2b2b3018d40134540fd3
Adrin63 <adrianriscolopez@gmail.com>
2024-10-02 17:10:13 +0200

    last updates

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
@@ -13,5 +13,12 @@ export class HeroesService {
     
     getHeroes():Observable<Hero[]> {
         return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+    }
+
+    getHeroById(id: string): Observable<Hero | undefined> {
+        return this.httpClient.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+        .pipe(
+            catchError( error => of(undefined))
+        );
     }
 }
